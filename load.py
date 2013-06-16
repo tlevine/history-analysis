823d365fe16a7ab95f1c761a9c9e80aca971d1a4
Thomas Levine <occurrence@thomaslevine.com>
2013-06-16 07:01:08 -0700

    extract features from individual files

--- load.py
+++ load.py
@@ -8,14 +8,38 @@ import pandas
 HISTORY = os.path.join(os.environ['HOME'], '.history')
 
 def filename_to_session(filename):
-    'unicode -> (unicode, datetime.date)'
+    'unicode -> (unicode, unicode, datetime.date)'
     s = re.split(r'[- T]', filename.replace('r-log', 'rlog'))
-    return s[0], datetime.date(*map(int, s[1:4]))
+    return filename, s[0], datetime.date(*map(int, s[1:4]))
 
 def sessions_by_day():
     'IO () -> [session]'
     return [filename_to_session(filename) for filename in filter(lambda filename: '--' not in filename and '-' in filename, os.listdir(HISTORY))]
 
-df = pandas.DataFrame(sessions_by_day(), columns = ['language', 'day'])
+def fileinfo(filename):
+    'unicode -> (int)'
+    path = os.path.join(HISTORY, filename)
 
-days = df[df['language'] == 'sh'].groupby('day')['day'].count()
+    handle = open(path)
+    lines = handle.read().split('\n')
+    handle.close()
+
+    fts = datetime.datetime.fromtimestamp
+    return pandas.Series({
+        'filename': filename,
+        'bytes': os.stat(path).st_size,
+        'commands': len(lines)/2,
+        'first_command_run': fts(float(lines[0][1:])),
+        'last_command_run': fts(float(lines[-3][1:])) if len(lines) > 3 else None,
+    })
+
+# Extract stuff from the file information
+df = pandas.DataFrame(sessions_by_day(), columns = ['filename', 'language', 'day'])
+df.index = df['filename']
+
+# Only shell
+df = df[df['language'] == 'sh']
+
+# Extract stuff from the file.
+df = df.head(10)
+df = df.merge(df['filename'].apply(fileinfo))
