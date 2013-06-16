a3c1cc1e70e08af6934ab979e4c2ce80db7ce93b
Thomas Levine <occurrence@thomaslevine.com>
2013-06-16 07:21:23 -0700

    commands

--- load.py
+++ load.py
@@ -7,6 +7,9 @@ import pandas
 
 HISTORY = os.path.join(os.environ['HOME'], '.history')
 
+def fts(commented_timestamp):
+    return datetime.datetime.fromtimestamp(float(commented_timestamp[1:]))
+
 def filename_to_session(filename):
     'unicode -> (unicode, unicode, datetime.date)'
     s = re.split(r'[- T]', filename.replace('r-log', 'rlog'))
@@ -16,30 +19,49 @@ def sessions_by_day():
     'IO () -> [session]'
     return [filename_to_session(filename) for filename in filter(lambda filename: '--' not in filename and '-' in filename, os.listdir(HISTORY))]
 
-def fileinfo(filename):
-    'unicode -> (int)'
+def open_history(filename):
     path = os.path.join(HISTORY, filename)
+    return open(path, 'r')
 
-    handle = open(path)
+def file_info(filename):
+    'unicode -> (int)'
+    handle = open_history(filename)
     lines = handle.read().split('\n')
     handle.close()
 
-    fts = datetime.datetime.fromtimestamp
     return pandas.Series({
         'filename': filename,
         'bytes': os.stat(path).st_size,
         'commands': len(lines)/2,
-        'first_command_run': fts(float(lines[0][1:])),
-        'last_command_run': fts(float(lines[-3][1:])) if len(lines) > 3 else None,
+        'first_command_run': fts(lines[0]),
+        'last_command_run': fts(lines[-3]) if len(lines) >= 3 else None,
     })
 
-# Extract stuff from the file information
-df = pandas.DataFrame(sessions_by_day(), columns = ['filename', 'language', 'day'])
-df.index = df['filename']
+def file_features():
+    # Extract stuff from the file information
+    df = pandas.DataFrame(sessions_by_day(), columns = ['filename', 'language', 'day'])
+    df.index = df['filename']
 
-# Only shell
-df = df[df['language'] == 'sh']
+    # Only shell
+    df = df[df['language'] == 'sh']
 
-# Extract stuff from the file.
-df = df.head(10)
-df = df.merge(df['filename'].apply(fileinfo))
+    # Extract stuff from the file.
+    # df = df.head(10)
+    # df = df.merge(df['filename'].apply(file_info))
+
+    return df
+
+def command_info(filename):
+    handle = open_history(filename)
+    lines = handle.read().split('\n')
+    handle.close()
+
+    timestamps = map(fts, lines[0::2][:-1])
+    commands = lines[1::2]
+    return zip(timestamps, commands)
+
+def command_features(filenames_series):
+    filenames_series.apply()
+
+file_df    = file_features()
+# command_df = command_features(filenames['filenames'])
